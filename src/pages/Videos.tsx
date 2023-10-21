53842650041ed556ed048f6b2b14be664e346112
Alan <alkalinemusic@gmail.com>
2023-10-21 15:32:12 -0400

    style video playlist and player some

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -5,6 +5,7 @@ import "./Videos.css";
 interface Video {
   name: string;
   path: string;
+  description: string;
 }
 
 const Videos: React.FC = () => {
@@ -16,26 +17,31 @@ const Videos: React.FC = () => {
     {
       name: "Hotel Vernon | Live Set Techno | 10 07 2023",
       path: "https://youtu.be/FR82wqT8RFU?si=V89xTXAHafPnDH7r",
+      description: "An electric techno set performed at Hotel Vernon.",
     },
     {
       name: "AS220 | Live Set Techno | 08 10 2023",
       path: "https://youtu.be/GE6b4F04NDI?si=b5WdswfBgjpeYtQ2",
+      description: "Captivating techno rhythms at AS220.",
     },
     {
       name: "Fluid Machina | Live Set Ambient | 09 17 2023",
       path: "https://youtu.be/9VyPo4li2B8?si=tSNbndPHjXz7Eu2W",
+      description: "A soothing ambient set that sets the mood",
     },
     {
       name: "Mayday Open Mic | Live Set Techno 07 20",
       path: "https://youtu.be/SKvAW1VV16Y?si=0J6JEAk0paFdTir3",
+      description: "An energetic open mic techno set that lit up Mayday.",
     },
     {
       name: "Mayday Live Set Techno 10 19 2023",
       path: "https://youtu.be/9ws9YuBmroc?si=QDpbdKM7ntQP57NQ",
+      description:
+        "A pumping techno set that kept the dance floor alive at Mayday.",
     },
   ];
 
-  // Load the first video when the component mounts
   useEffect(() => {
     if (videos.length > 0) {
       setSelectedVideo(videos[0].path);
@@ -52,51 +58,29 @@ const Videos: React.FC = () => {
   };
 
   return (
-    <div
-      style={{
-        marginTop: "110px",
-        display: "flex",
-        justifyContent: "center", // Center the content horizontally
-        alignItems: "flex-start", // Align content to the top vertically
-      }}
-    >
+    <div className="video-container">
       {isVideoLoaded && (
         <div
-          style={{
-            textAlign: "center",
-            width: "50%",
-            overflow: "hidden", // Hide overflowing controls
-          }}
+          className="video-player"
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         >
           <div style={{ width: "100%" }}>
             <ReactPlayer
               url={selectedVideo}
-              controls={showControls} // Show controls based on showControls state
+              controls={showControls}
               width="100%"
             />
           </div>
         </div>
       )}
-
-      <div style={{ textAlign: "center", marginLeft: "20px" }}>
+      <div className="video-list">
         <h2>Video List</h2>
-        <ul
-          style={{
-            listStyleType: "none",
-            padding: 0,
-            marginLeft: "auto",
-            marginRight: "auto",
-          }}
-        >
+        <ul>
           {videos.map((video, index) => (
-            <li
-              key={index}
-              onClick={() => setSelectedVideo(video.path)}
-              style={{ marginBottom: "10px" }}
-            >
-              {video.name}
+            <li key={index} onClick={() => setSelectedVideo(video.path)}>
+              <div className="video-title">{video.name}</div>
+              <div className="video-description">{video.description}</div>
             </li>
           ))}
         </ul>
